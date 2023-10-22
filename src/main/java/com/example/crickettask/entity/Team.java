86d0544bfe5fbf0b5b38cb639e49d22478fff559
Venkatesh Selvam <venkateshselvam1998@gmail.com>
2023-10-22 16:53:07 +0530

    phase1

---
+++ src/main/java/com/example/crickettask/entity/Team.java
@@ -0,0 +1,24 @@
+package com.example.crickettask.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document(collection = "teams")
+public class Team {
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String teamname;
+
+    //private Integer teamsize;
+
+}
---
+++ src/test/java/com/example/crickettask/CricketTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crickettask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
