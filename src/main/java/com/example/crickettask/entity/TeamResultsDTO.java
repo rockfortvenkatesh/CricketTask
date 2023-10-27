8e51a27bbeee21d55937fc0ba031d1b4feea2a8d
Venkatesh Selvam <venkateshselvam1998@gmail.com>
2023-10-27 11:20:16 +0530

    Result

--- src/main/java/com/example/crickettask/entity/TeamResultsDTO.java
+++ src/main/java/com/example/crickettask/entity/TeamResultsDTO.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class TeamResultsDTO {
+    private String teamA;
+    private String teamB;
+    private LocalDate matchDate;
+    private LocalTime matchTime;
     private List<TeamResults> teamResults;
     private String winner;
 }
