ffd7f1af8eb6b5276e591242ed3f38598105b56a
Venkatesh Selvam <venkateshselvam1998@gmail.com>
2023-11-01 18:52:54 +0530

    Add Swagger API

--- src/main/java/com/example/crickettask/controller/MatchController.java
+++ src/main/java/com/example/crickettask/controller/MatchController.java
@@ -3,6 +3,9 @@ package com.example.crickettask.controller;
 import com.example.crickettask.entity.Match;
 import com.example.crickettask.entity.TeamResultsDTO;
 import com.example.crickettask.service.MatchService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/match")
+@Tag(
+        name = "Match Controller API",
+        description = "This is the class that implements api related Match "
+)
 public class MatchController {
 
     private final MatchService matchService;
@@ -19,17 +26,33 @@ public class MatchController {
     public MatchController(MatchService matchService) {
         this.matchService = matchService;
     }
-
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status code 200 ok"
+    )
+    @Operation(
+            summary = "Finding all Match from DB",
+            description = "This API if find All Match from DB"
+    )
     @GetMapping("/getAllMatch")
     public List<Match> getAllmatch(){
         return matchService.getAllMatch();
     }
+
+    @Operation(
+            summary = "Start the Match",
+            description = "This API if start the match and store the result to DB"
+    )
     @PostMapping("/startmatch")
     public ResponseEntity<Match> startMatch(@RequestParam String TeamAId, @RequestParam String TeamBId, @RequestParam int Over ,@RequestParam int PlayerSize ){
         Match match  = matchService.StartMatch(TeamAId,TeamBId, Over , PlayerSize);
         return ResponseEntity.status(HttpStatus.CREATED).body(match);
     }
 
+    @Operation(
+            summary = "Finding Match result from DB",
+            description = "This API if find match result in specific ID from DB"
+    )
     @GetMapping("/results/{matchId}")
     public ResponseEntity<TeamResultsDTO> getMatchResults(@PathVariable String matchId) {
         TeamResultsDTO matchResult = matchService.getMatchResults(matchId);
---
+++ src/test/java/com/example/crickettask/TeamTest.java
@@ -0,0 +1,13 @@
+package com.example.crickettask;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TeamTest {
+
+    @Test
+    public  void demotest(){
+        assertTrue(true);
+    }
+}
